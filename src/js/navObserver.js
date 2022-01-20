59d3b459e1173b8fe7e9a5d78badf91aa4a00224
veeru-neerukonda <veeru.neerukonda@gmail.com>
2022-01-20 15:05:25 +0530

    nav-bar Observer root margin changing on resize bug fixed.

--- src/js/navObserver.js
+++ src/js/navObserver.js
@@ -1,27 +1,29 @@
 export function generateNavObserver(observerMain){
-    //assuming navBar is 75pixels .. there is no reason to assume this and the code will likely break in the future
-    let observerMarginBottom = window.innerHeight - 75;
-
-    let observer = new IntersectionObserver(observerMain,{
-        root: null,
-        rootMargin: `0px 0px -${observerMarginBottom}px 0px`,
-    });
-
-    window.addEventListener("resize", function(){
+    let observer = null;
+    
+    let generate = function(){
         //assuming navBar is 75pixels .. there is no reason to assume this and the code will likely break in the future
         let observerMarginBottom = window.innerHeight - 75;
-        observer.rootMargin = `0px 0px -${observerMarginBottom}px 0px`;
+
+        observer = new IntersectionObserver(observerMain,{
+            root: null,
+            rootMargin: `0px 0px -${observerMarginBottom}px 0px`,
+        });
+
+        let homeSection = document.getElementById('home-section');
+        let portfolioSection = document.getElementById('portfolio-section-complete');
+        let hireMeSection = document.getElementById('hire-me-section-complete');
+        let contactMeSection = document.getElementById('contact-me-section-complete');
+
+        observer.observe(homeSection);
+        observer.observe(portfolioSection);
+        observer.observe(hireMeSection);
+        observer.observe(contactMeSection);
+    }
+
+    generate();
+    window.addEventListener("resize", function(){
+        observer.disconnect();
+        generate();
     })
-
-    let homeSection = document.getElementById('home-section');
-    let portfolioSection = document.getElementById('portfolio-section-complete');
-    let hireMeSection = document.getElementById('hire-me-section-complete');
-    let contactMeSection = document.getElementById('contact-me-section-complete');
-
-    observer.observe(homeSection);
-    observer.observe(portfolioSection);
-    observer.observe(hireMeSection);
-    observer.observe(contactMeSection);
-
-    return observer;
 }
