21dfde1001bbc54197fc0c17198fdda2252e6925
Veeru <veeru.neerukonda@gmail.com>
2022-05-15 09:21:50 +0530

    standradized the phat cards.

--- src/components/PhatCards/BlogWebsitePhatCard.vue
+++ src/components/PhatCards/BlogWebsitePhatCard.vue
@@ -58,38 +58,38 @@
             require('@/assets/images/project-blog-4.png'),
         ]"
     >
-        <template #projectDescription>
-            <p class="strong text-center text-small">
-                This fully responsive website written using <a class="link" href="https://v3.vuejs.org/">Vue3</a> packs a pretty punch with its impressive features like:                            
-            </p>
-            <ul class="project-description-list">
-                <li>
-                    <p class="bodyText">
-                        Fully featured searching with the capability to <a class="link" href="https://fusejs.io/">Fuzzy Search</a> and result filter using <strong>Custom Tags</strong> easily declarable by a non programmer in a seperate json file.
-                    </p>
-                </li>
-                <li>
-                    <p class="bodyText">
-                        The integration of a component library in the form of <a class="link" href="https://www.naiveui.com/en-US/os-theme">Naive UI</a> allows me to quickly and correctly implement complex Ui elements which otherwise would have taken far too long to write anf test for an acceptable production level performance.
-                    </p>
-                </li>
-                <li>
-                    <p class="bodyText">
-                        <a class="link" href="https://router.vuejs.org/">Vue router</a> coupled with <a class="link" href="https://v3.vuejs.org/guide/transitions-enterleave.html#transitioning-single-elements-components">transitions using Vue3</a> allows for seamless transition between different pages of the blog all the while retaining the feel of a single page application.
-                    </p>
-                </li>
-            </ul>
-            <div class="sticker-box">
-                <Icon size="64" color="#262525">
-                    <LogoVue />
-                </Icon>
-                <Icon size="64" color="#262525">
-                    <Json />
-                </Icon>
-                <Icon size="64" color="#262525">
-                    <Js />
-                </Icon>
-            </div>
+        <template #project-description-heading>
+            This fully responsive website written using <a class="link" href="https://v3.vuejs.org/">Vue3</a> packs a pretty punch with its impressive features like: 
+        </template>
+
+        <template #project-description-list>
+            <li>
+                <p class="bodyText">
+                    Fully featured searching with the capability to <a class="link" href="https://fusejs.io/">Fuzzy Search</a> and result filter using <strong>Custom Tags</strong> easily declarable by a non programmer in a seperate json file.
+                </p>
+            </li>
+            <li>
+                <p class="bodyText">
+                    The integration of a component library in the form of <a class="link" href="https://www.naiveui.com/en-US/os-theme">Naive UI</a> allows me to quickly and correctly implement complex Ui elements which otherwise would have taken far too long to write anf test for an acceptable production level performance.
+                </p>
+            </li>
+            <li>
+                <p class="bodyText">
+                    <a class="link" href="https://router.vuejs.org/">Vue router</a> coupled with <a class="link" href="https://v3.vuejs.org/guide/transitions-enterleave.html#transitioning-single-elements-components">transitions using Vue3</a> allows for seamless transition between different pages of the blog all the while retaining the feel of a single page application.
+                </p>
+            </li>
+        </template>
+
+        <template #sticker-box>
+            <Icon size="64" color="#262525">
+                <LogoVue />
+            </Icon>
+            <Icon size="64" color="#262525">
+                <Json />
+            </Icon>
+            <Icon size="64" color="#262525">
+                <Js />
+            </Icon>
         </template>
     </PhatProjectCard>
 </template>
@@ -98,24 +98,8 @@
 @use '@/assets/scss/setting' as *;
 @import '@/sections/_section.scss';
 
-.link{
-    text-decoration: none;
-    color: map-get($accent,"light-mode");
-    font-weight: 400;
-}
-
-.project-description-list{
-    list-style:square;
-    li{
-        margin-top: var(--spacing-normal);
-    }
-}
-
-.sticker-box{
-    display: flex;
-    flex-direction: row;
-    justify-content: space-around;
-    width: 70%;
+li {
+    margin-top: var(--spacing-normal);
 }
 </style>
 
--- src/components/PhatCards/TicTacToePhatCard.vue
+++ src/components/PhatCards/TicTacToePhatCard.vue
@@ -32,39 +32,39 @@
             require('@/assets/images/ai-tic-tac-toe-6.png'),
         ]"
     >
-        <template #projectDescription>
-            <p class="strong text-center text-small">
-                This fullstack web game uses both <a class="link" href="https://www.heroku.com/platform">Heroku</a> and <a class="link" href="https://www.netlify.com/">Netlify</a> together to provide blazing fast client load times as well as secure access to a transactional database.
-            </p>
-            <ul class="project-description-list">
-                <li>
-                    <p class="bodyText">
-                        A <a class="link" href="https://en.wikipedia.org/wiki/Static_web_page">Static Page</a> is served to the client with exceptional speed using the <a href="https://azure.microsoft.com/en-in/overview/what-is-paas/" class="link">Platform As A Service (PAAS)</a> provider <a class="link" href="https://www.netlify.com/">Netlify</a>. 
-                    </p>
-                </li>
-                <li>
-                    <p class="bodyText">
-                        This <a class="link" href="https://en.wikipedia.org/wiki/Static_web_page">Static Page</a> is invested with the responsibility to conduct and proctor the game. Client computation resources are utilized as much as possible so as to keep operation costs of the server low.
-                    </p>
-                </li>
-                <li>
-                    <p class="bodyText">
-                        The server hosted on the platform <a class="link" href="https://www.heroku.com/platform">Heroku</a> is responsible for storing and maintaining player records using a <a href="https://www.mongodb.com/databases/types/transactional-databases" class="link">Transactional Database</a> .
-                    </p>
-                </li>
-                <li>
-                    <p class="bodyText">
-                        The <a href="https://www.redhat.com/en/topics/api/what-is-a-rest-api" class="link">Rest Api</a> implemented by the server hosted on <a class="link" href="https://www.heroku.com/platform">Heroku</a> is used as an interface for the client to access player records while the game is in session in the browser.  
-                    </p>
-                </li>
-            </ul>
-            <div class="sticker-box">
-                <NetlifyIcon />
-                <Icon size="64" color="#262525">
-                    <BoardGames20Filled />
-                </Icon>
-                <HerokuIcon />
-            </div>
+       <template #project-description-heading>
+           This fullstack web game uses both <a class="link" href="https://www.heroku.com/platform">Heroku</a> and <a class="link" href="https://www.netlify.com/">Netlify</a> together to provide blazing fast client load times as well as secure access to a transactional database.
+        </template>
+
+        <template #project-description-list>
+            <li>
+                <p class="bodyText">
+                    A <a class="link" href="https://en.wikipedia.org/wiki/Static_web_page">Static Page</a> is served to the client with exceptional speed using the <a href="https://azure.microsoft.com/en-in/overview/what-is-paas/" class="link">Platform As A Service (PAAS)</a> provider <a class="link" href="https://www.netlify.com/">Netlify</a>. 
+                </p>
+            </li>
+            <li>
+                <p class="bodyText">
+                    This <a class="link" href="https://en.wikipedia.org/wiki/Static_web_page">Static Page</a> is invested with the responsibility to conduct and proctor the game. Client computation resources are utilized as much as possible so as to keep operation costs of the server low.
+                </p>
+            </li>
+            <li>
+                <p class="bodyText">
+                    The server hosted on the platform <a class="link" href="https://www.heroku.com/platform">Heroku</a> is responsible for storing and maintaining player records using a <a href="https://www.mongodb.com/databases/types/transactional-databases" class="link">Transactional Database</a> .
+                </p>
+            </li>
+            <li>
+                <p class="bodyText">
+                    The <a href="https://www.redhat.com/en/topics/api/what-is-a-rest-api" class="link">Rest Api</a> implemented by the server hosted on <a class="link" href="https://www.heroku.com/platform">Heroku</a> is used as an interface for the client to access player records while the game is in session in the browser.  
+                </p>
+            </li>
+        </template>
+
+        <template #sticker-box>
+            <NetlifyIcon />
+            <Icon size="64" color="#262525">
+                <BoardGames20Filled />
+            </Icon>
+            <HerokuIcon />
         </template>
     </PhatProjectCard>
 </template>
@@ -73,24 +73,8 @@
 @use '@/assets/scss/setting' as *;
 @import '@/sections/_section.scss';
 
-.link{
-    text-decoration: none;
-    color: map-get($accent,"light-mode");
-    font-weight: 400;
-}
-
-.project-description-list{
-    list-style:square;
-    li{
-        margin-top: var(--spacing-normal);
-    }
-}
-
-.sticker-box{
-    display: flex;
-    flex-direction: row;
-    justify-content: space-around;
-    width: 70%;
+li{
+    margin-top: var(--spacing-normal);
 }
 </style>
 
